3e0d90ab7ae9875c3f3b882bb3a53d8cf1c127d8
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2019-12-28 09:49:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,17 @@
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end 
+ introduction ("Jurabek") 
 def introduction_with_language(name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 introduction_with_language("Ruby")
 
+
+def introduction_with_language_optional(name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language_optional("Ruby")
 
 
 
